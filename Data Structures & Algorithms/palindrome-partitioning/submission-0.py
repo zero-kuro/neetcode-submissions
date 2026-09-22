081188b0e5761510a81818799d7f2f7488c517fb
zeronddd <155807824+zero-kuro@users.noreply.github.com>
2026-09-22 18:44:32 +0800

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        cur = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(cur.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPalin(s, i, j):
+                    cur.append(s[i:j+1])
+                    dfs(j+1)
+                    cur.pop()
+        dfs(0)
+        return res
+
+
+    def isPalin(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+        
